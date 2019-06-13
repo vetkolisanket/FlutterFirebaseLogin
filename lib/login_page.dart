b0ea71787ff07097727a8fbb30e1a7a7968840b5
Sanket <vetkolisanket@gmail.com>
2019-06-13 21:56:33 +0530

    Completed login flow

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_firebase_auth/auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -39,17 +41,42 @@ class _LoginPageState extends State<LoginPage> {
                     decoration: InputDecoration(labelText: "Password"),
                   ),
                   SizedBox(height: 20),
-                  RaisedButton(child: Text("LOGIN"), onPressed: () {
+                  RaisedButton(child: Text("LOGIN"), onPressed: () async {
                     final form = _formKey.currentState;
                     form.save();
 
                     // Validate will return true if is valid, or false if invalid.
                     if (form.validate()) {
-                      print("$_email $_password");
+                      var result = await Provider.of<AuthService>(context).loginUser(
+                        email: _email, password: _password
+                      );
+                      if (result == null) {
+                        return _buildErrorDialog(context, "Incorrect password!");
+                      }
                     }
                   }),
                 ],
               ),
             )));
+  }
+
+  Future _buildErrorDialog(BuildContext context, _message) {
+    return showDialog(
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Error Message'),
+          content: Text(_message),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+      context: context,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:simple_firebase_auth/auth.dart';
 import 'package:simple_firebase_auth/home_page.dart';
 import 'package:simple_firebase_auth/login_page.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+    ChangeNotifierProvider<AuthService>(
+      child: MyApp(),
+      builder: (BuildContext context) {
+        return AuthService();
+      },
+    )
+);
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-//      home: LoginPage(),
-      home: HomePage(),
+      home: FutureBuilder(
+          future: Provider.of<AuthService>(context).getUser(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return snapshot.hasData ? HomePage() : LoginPage();
+            } else {
+              return Container(color: Colors.white);
+            }
+          }
+      ),
     );
   }
 }
